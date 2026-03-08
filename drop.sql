f45a30d13a6f3a70ec450367fd3e3e9e7b2a8be1
Alex Politis <101974430+mastralexis@users.noreply.github.com>
2026-03-08 20:48:31 +0200

    Add files via upload

---
+++ drop.sql
@@ -0,0 +1,11 @@
+-- Drop all tables in the database in the correct order to avoid foreign key constraints
+DROP TABLE Writes_Script;
+DROP TABLE Directs;
+DROP TABLE Produces;
+DROP TABLE Professional_Properties;
+DROP TABLE Keywords;
+DROP TABLE Genres;
+DROP TABLE Role;
+DROP TABLE Movie;
+DROP TABLE Staff_Member;
+DROP TABLE Company;
